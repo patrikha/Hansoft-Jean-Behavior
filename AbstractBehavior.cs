4909bd63e9e687f76368f4265fd3c7cad8a32248
Svante Lidman <svante.lidman@gmail.com>
2013-06-03 14:31:23 +0200

    First version on GitHub.

---
+++ AbstractBehavior.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using HPMSdk;
+using Hansoft.SimpleLogging;
+
+namespace Hansoft.Jean.Behavior
+{
+    /// <summary>
+    /// The base class for behaviors that should be automated by Jean for Hansoft. A behavior is notified of changes in Hansoft
+    /// by overriding the suitable event handlers in this base class and then making updates to Hansoft or take other actions
+    /// as required. To add a behavior to Jean you need to do the following
+    /// 1. Realize the behavior as a subclass of AbstractBehavior. The class and the dll should have the same name as your
+    ///    behavior, e.g., MyCustomBehavior.
+    /// 2. Add the behavior to the directory where Jean.exe is found
+    /// 3. Add your behavior dll to the LoadAssemblies section in JeanSettings.xml
+    /// 4. Instantiate your behavior by adding a behavior element with any needed parameters to JeanSettings.xml
+    ///    (See the provided examples for details).
+    /// </summary>
+    public abstract class AbstractBehavior
+    {
+        XmlElement configuration;
+        bool bufferedEvents;
+        bool initialized;
+        List<string> extensionAssemblies;
+        ILogger logger;
+
+        /// <summary>
+        /// General constructor. Should be called by subclasses.
+        /// </summary>
+        /// <param name="configuration">The XmlElement that contains the configuration parameters for the behavior.</param>
+        public AbstractBehavior(XmlElement configuration)
+        {
+            this.configuration = configuration;
+            this.initialized = false;
+        }
+
+        /// <summary>
+        /// Subclasses should return a descriptive string of the behavior instance used, e.g., for debugging/tracing.
+        /// </summary>
+        public abstract string Title { get; }
+
+        /// <summary>
+        /// Returns the value of the specified XmlAttribute from the configuration XmlElement.
+        /// </summary>
+        /// <param name="parameterName">Name of the requested XmlAttribure.</param>
+        /// <returns>The value of the XmlAttribute if it exists. Otherwise string.Empty is returned.</returns>
+        public string GetParameter(string parameterName)
+        {
+            if (configuration.HasAttribute(parameterName))
+            {
+                return configuration.GetAttribute(parameterName).Replace('\'', '"');
+            }
+            else
+                return string.Empty;
+        }
+
+        /// <summary>
+        /// This method is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="bufferedEvents">Indicates whether events should be buffered or not.</param>
+        /// <param name="extensionAssemblies">A list of paths for the assemblies that have been loaded in the LoadAssemblies section of JeanSettings.xml</param>
+        /// <param name="logger">The logging service instance to be used.</param>
+        public void Initialize(bool bufferedEvents, List<string> extensionAssemblies, ILogger logger)
+        {
+            this.logger = logger;
+            this.bufferedEvents = bufferedEvents;
+            this.extensionAssemblies = extensionAssemblies;
+            initialized = false;
+            Initialize();
+            initialized = true;
+        }
+
+        /// <summary>
+        /// Subclass responsibility, should throw an exception if initialization fails in some way.
+        /// </summary>
+        /// <returns></returns>
+        public abstract void Initialize();
+
+        /// <summary>
+        /// Indicates whether the behavior has been succesfully initialized or not. You should not set this explicitly as it
+        /// is handled by the Jean for Hansoft framework.
+        /// </summary>
+        public bool Initialized
+        {
+            get{ return initialized; }
+            set { initialized = value;}
+        }
+
+        /// <summary>
+        /// Indicates whether Jean for Hansoft is currently buffering events as specified with the EventWindow attribute of the Connection element in JeanSettings.xml
+        /// </summary>
+        public bool BufferedEvents
+        {
+            get { return bufferedEvents; }
+        }
+
+        /// <summary>
+        /// The list of paths for the assemblies that have been loaded in the LoadAssemblies section of JeanSettings.xml
+        /// </summary>
+        public List<string> ExtensionAssemblies
+        {
+            get { return extensionAssemblies; }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnTaskChange(object sender, TaskChangeEventArgs e)
+        {
+            try
+            {
+                OnTaskChange(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing TaskChangeEvent for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to handle TaskChange events.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnTaskChange(TaskChangeEventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnTaskChangeCustomColumnData(object sender, TaskChangeCustomColumnDataEventArgs e)
+        {
+            try
+            {
+                OnTaskChangeCustomColumnData(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing TaskChangeCustomColumnData for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to handle TaskChangeCustomColumnData events.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnTaskChangeCustomColumnData(TaskChangeCustomColumnDataEventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnTaskMove(object sender, TaskMoveEventArgs e)
+        {
+            try
+            {
+                OnTaskMove(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing TaskMove for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to handle TaskMove events.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnTaskMove(TaskMoveEventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnTaskDelete(object sender, TaskDeleteEventArgs e)
+        {
+            try
+            {
+                OnTaskDelete(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing TaskDelete for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to handle TaskDelete events.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnTaskDelete(TaskDeleteEventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnTaskCreate(object sender, TaskCreateEventArgs e)
+        {
+            try
+            {
+                OnTaskCreate(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing TaskCreateEvent for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to handle TaskCreate events.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnTaskCreate(TaskCreateEventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnBeginProcessBufferedEvents(object sender, EventArgs e)
+        {
+            try
+            {
+                OnBeginProcessBufferedEvents(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing BeginProcessBufferedEventsEvent for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to insert behavior before the processing of 
+        /// buffered events. This is only applicable if Jean for Hansoft is set up to use event buffering.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnBeginProcessBufferedEvents(EventArgs e) { }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnEndProcessBufferedEvents(object sender, EventArgs e)
+        {
+            try
+            {
+                OnEndProcessBufferedEvents(e);
+            }
+            catch (Exception ex)
+            {
+                logger.Exception("Error proccessing EndProcessBufferedEventsEvent for behavior " + Title + ".", ex);
+            }
+        }
+
+        /// <summary>
+        /// Override this function in subclasses to insert behavior after the processing of 
+        /// buffered events. This is only applicable if Jean for Hansoft is set up to use event buffering.
+        /// </summary>
+        /// <param name="e">The details of the event.</param>
+        public virtual void OnEndProcessBufferedEvents(EventArgs e) { }
+    }
+}
---
+++ HPMCallbackHandler.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading;
+using System.Xml;
+
+using HPMSdk;
+
+namespace Hansoft.Jean.Behavior
+{
+    /// <summary>
+    /// EventArgs subclass for Hansoft TaskChange events
+    /// </summary>
+    public class TaskChangeEventArgs : EventArgs
+    {
+        private HPMChangeCallbackData_TaskChange data;
+
+        internal TaskChangeEventArgs(HPMChangeCallbackData_TaskChange data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public HPMChangeCallbackData_TaskChange Data { get { return data; } }
+    }
+
+    /// <summary>
+    /// EventArgs subclass for Hansoft TaskChangeCustomColumnData events
+    /// </summary>
+    public class TaskChangeCustomColumnDataEventArgs : EventArgs
+    {
+        private HPMChangeCallbackData_TaskChangeCustomColumnData data;
+
+        internal TaskChangeCustomColumnDataEventArgs(HPMChangeCallbackData_TaskChangeCustomColumnData data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public HPMChangeCallbackData_TaskChangeCustomColumnData Data { get { return data; } }
+    }
+
+    /// <summary>
+    /// EventArgs subclass for Hansoft TaskChangeDisposition events
+    /// </summary>
+    public class TaskMoveEventArgs : EventArgs
+    {
+        private HPMChangeCallbackData_TaskChangeDisposition data;
+
+        internal TaskMoveEventArgs(HPMChangeCallbackData_TaskChangeDisposition data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public HPMChangeCallbackData_TaskChangeDisposition Data { get { return data; } }
+    }
+
+    /// <summary>
+    /// EventArgs subclass for Hansoft TaskCreate events
+    /// </summary>
+    public class TaskCreateEventArgs : EventArgs
+    {
+        private HPMChangeCallbackData_TaskCreateUnified data;
+
+        internal TaskCreateEventArgs(HPMChangeCallbackData_TaskCreateUnified data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public HPMChangeCallbackData_TaskCreateUnified Data { get { return data; } }
+    }
+
+    /// <summary>
+    /// EventArgs subclass for Hansoft TaskDelete events
+    /// </summary>
+    public class TaskDeleteEventArgs : EventArgs
+    {
+        private HPMChangeCallbackData_TaskDelete data;
+
+        internal TaskDeleteEventArgs(HPMChangeCallbackData_TaskDelete data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public HPMChangeCallbackData_TaskDelete Data { get { return data; } }
+    }
+
+    /// <summary>
+    /// EventArgs subclass for Hansoft ProcessError events
+    /// </summary>
+    public class ProcessErrorEventArgs : EventArgs
+    {
+        private EHPMError error;
+
+        internal ProcessErrorEventArgs(EHPMError error)
+        {
+            this.error = error;
+        }
+
+        /// <summary>
+        /// The detailed event data.
+        /// </summary>
+        public EHPMError Error { get { return error; } }
+    }
+
+    /// <summary>
+    /// This class is internal to the Jean for Hansoft framework and should not be instantiated directly.
+    /// </summary>
+    public class HPMCallbackHandler : HPMSdkCallbacks, IDisposable
+    {
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<TaskChangeEventArgs> TaskChange;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<TaskChangeCustomColumnDataEventArgs> TaskChangeCustomColumnData;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<TaskCreateEventArgs> TaskCreate;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<TaskDeleteEventArgs> TaskDelete;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<TaskMoveEventArgs> TaskMove;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<ProcessErrorEventArgs> ProcessError;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<EventArgs> BeginProcessBufferedEvents;
+
+        /// <summary>
+        /// This event handler is internal to the Jean for Hansoft framework and you should not hook up to it directly.
+        /// To handle events you should create a subclass of AbstractBehvaior and override the relevant handling methods.
+        /// </summary>
+        public event EventHandler<EventArgs> EndProcessBufferedEvents;
+
+        private System.Timers.Timer windowTimer;
+        private int eventWindow;
+        private List<EventArgs> eventBuffer;
+
+        /// <summary>
+        /// This constructor is internal to the Jean for Hansoft framework and should not be invoked directly.
+        /// </summary>
+        /// <param name="eventWindow"></param>
+        public HPMCallbackHandler(int eventWindow)
+        {
+            this.eventWindow = eventWindow;
+            if (BufferEvents)
+            {
+                eventBuffer = new List<EventArgs>();
+                windowTimer = new System.Timers.Timer(eventWindow) { AutoReset = true };
+                windowTimer.Elapsed += (sender, eventArgs) => ProcessBufferedEvents();
+                windowTimer.Start();
+            }
+        }
+
+        private void ProcessBufferedEvents()
+        {
+            lock (eventBuffer)
+            {
+                if (eventBuffer.Count > 0)
+                {
+                    if (BeginProcessBufferedEvents != null)
+                        BeginProcessBufferedEvents(this, EventArgs.Empty);
+                    for (int i = 0; i < eventBuffer.Count; i += 1)
+                    {
+                        EventArgs e = eventBuffer[i];
+                        if (e is TaskCreateEventArgs)
+                        {
+                            if (TaskCreate != null)
+                                TaskCreate(this, (TaskCreateEventArgs)e);
+                        }
+                        else if (e is TaskChangeEventArgs)
+                        {
+                            if (TaskChange != null)
+                                TaskChange(this, (TaskChangeEventArgs)e);
+                        }
+                        else if (e is TaskChangeCustomColumnDataEventArgs)
+                        {
+                            if (TaskChangeCustomColumnData != null)
+                                TaskChangeCustomColumnData(this, (TaskChangeCustomColumnDataEventArgs)e);
+                        }
+                        else if (e is TaskDeleteEventArgs)
+                        {
+                            if (TaskDelete != null)
+                                TaskDelete(this, (TaskDeleteEventArgs)e);
+                        }
+                        else if (e is TaskMoveEventArgs)
+                        {
+                            if (TaskMove != null)
+                                TaskMove(this, (TaskMoveEventArgs)e);
+                        }
+                    }
+                    if (EndProcessBufferedEvents != null)
+                        EndProcessBufferedEvents(this, EventArgs.Empty);
+                    eventBuffer.Clear();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        public void Dispose()
+        {
+            if (BufferEvents)
+            {
+                windowTimer.Stop();
+                eventBuffer.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether Jean for Hansoft is set up to buffer events or not.
+        /// </summary>
+        public bool BufferEvents
+        {
+            get { return eventWindow > 0; }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Error"></param>
+        public override void On_ProcessError(EHPMError _Error)
+        {
+            if (ProcessError != null)
+                ProcessError(this, new ProcessErrorEventArgs(_Error));
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Data">The detailed information of the change.</param>
+        public override void On_TaskChangeCustomColumnData(HPMChangeCallbackData_TaskChangeCustomColumnData _Data)
+        {
+            base.On_TaskChangeCustomColumnData(_Data);
+            if (BufferEvents)
+            {
+                lock (eventBuffer)
+                {
+                    eventBuffer.Add(new TaskChangeCustomColumnDataEventArgs(_Data));
+                }
+            }
+            else
+            {
+                if (TaskChangeCustomColumnData != null)
+                    TaskChangeCustomColumnData(this, new TaskChangeCustomColumnDataEventArgs(_Data));
+            }
+        }
+
+        private bool IgnoredField(EHPMTaskField field)
+        {
+            switch (field)
+            {
+                case (EHPMTaskField.Archived):
+                case (EHPMTaskField.AttachedDocuments):
+                case (EHPMTaskField.Color):
+                case (EHPMTaskField.CommentsOptions):
+                case (EHPMTaskField.Container):
+                case (EHPMTaskField.CreatedFromWorkflowObject):
+                case (EHPMTaskField.CreatedPipelineTasks):
+                case (EHPMTaskField.CustomColumnData):
+                case (EHPMTaskField.DefaultWorkflow):
+                case (EHPMTaskField.DelegateTo):
+                case (EHPMTaskField.ForceSubProject):
+                case (EHPMTaskField.FullyCreated):
+                case (EHPMTaskField.ID):
+                case (EHPMTaskField.IdealDaysHistory):
+                case (EHPMTaskField.LastUpdatedTime):
+                case (EHPMTaskField.LastUserInterfaceAction):
+                case (EHPMTaskField.LinkedToPipelineTask):
+                case (EHPMTaskField.LockedBy):
+                case (EHPMTaskField.LockedType):
+                case (EHPMTaskField.NewVersionOfSDKRequired):
+                case (EHPMTaskField.OriginallyCreatedBy):
+                case (EHPMTaskField.PointsHistory):
+                case (EHPMTaskField.SprintResourcesHaveFullRights):
+                case (EHPMTaskField.Undefined):
+                case (EHPMTaskField.Unlocked):
+                case (EHPMTaskField.VacationOptions):
+                case (EHPMTaskField.WallItemColor):
+                case (EHPMTaskField.WallItemPositions):
+                case (EHPMTaskField.VisibleTo):
+                case (EHPMTaskField.WorkRemainingHistory):
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Data">The detailed information of the change.</param>
+        public override void On_TaskChange(HPMChangeCallbackData_TaskChange _Data)
+        {
+            base.On_TaskChange(_Data);
+            if (!IgnoredField(_Data.m_FieldChanged))
+            {
+                if (BufferEvents)
+                {
+                    lock (eventBuffer)
+                    {
+                        eventBuffer.Add(new TaskChangeEventArgs(_Data));
+                    }
+                }
+                else
+                {
+                    if (TaskChange != null)
+                        TaskChange(this, new TaskChangeEventArgs(_Data));
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Data">The detailed information of the change.</param>
+        public override void On_TaskChangeDisposition(HPMChangeCallbackData_TaskChangeDisposition _Data)
+        {
+            base.On_TaskChangeDisposition(_Data);
+            if (BufferEvents)
+            {
+                lock (eventBuffer)
+                {
+                    eventBuffer.Add(new TaskMoveEventArgs(_Data));
+                }
+            }
+            else
+            {
+                if (TaskMove != null)
+                    TaskMove(this, new TaskMoveEventArgs(_Data));
+            }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Data">The detailed information of the change.</param>
+        public override void On_TaskCreateUnified(HPMChangeCallbackData_TaskCreateUnified _Data)
+        {
+            base.On_TaskCreateUnified(_Data);
+            if (BufferEvents)
+            {
+                lock (eventBuffer)
+                {
+                    eventBuffer.Add(new TaskCreateEventArgs(_Data));
+                }
+            }
+            else
+            {
+                if (TaskCreate != null)
+                    TaskCreate(this, new TaskCreateEventArgs(_Data));
+            }
+        }
+
+        /// <summary>
+        /// This function is internal to the Jean for Hansoft framework and should not be called directly.
+        /// </summary>
+        /// <param name="_Data">The detailed information of the change.</param>
+        public override void On_TaskDelete(HPMChangeCallbackData_TaskDelete _Data)
+        {
+            base.On_TaskDelete(_Data);
+            if (BufferEvents)
+            {
+                lock (eventBuffer)
+                {
+                    eventBuffer.Add(new TaskDeleteEventArgs(_Data));
+                }
+            }
+            else
+            {
+                if (TaskDelete != null)
+                    TaskDelete(this, new TaskDeleteEventArgs(_Data));
+            }
+        }
+    }
+}
